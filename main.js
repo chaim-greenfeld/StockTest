44f40951107255021a1d063f55d74c6573316d37
yossi <ix44248@gmail.com>
2025-12-11 12:37:12 +0200

    finished true game

--- main.js
+++ main.js
@@ -7,19 +7,19 @@ let askPupular = Number(input("enter 1 or 2 or 3 or 4:   " ))
 while (askPupular !== "exit"){
     if (askPupular === 1){
         let identifier1 = input("enter a name of stock or id of stock  ")
-        console.log(searchStock(identifier1))
+        console.log(searchStock(identifier1), "\n Did we find the stock???")
         
     }
     if (askPupular === 2){
         let givenPrice1 = Number(input("enter a price  "))
         let above1 = input("enter true if above or false if less  ")
-        console.log(filterStocksByPrice(givenPrice1 , above1), "is dede")
+        console.log(filterStocksByPrice(givenPrice1 , above1), "\n This is what I found.")
         
     }
     if (askPupular === 3){
         let operation1 = input("enter buy or sell  ")
         let identifier1 = input("enter a name of stock or id of stock  ")
-        console.log(OperateOnStock(operation1, identifier1))
+        console.log(OperateOnStock(operation1, identifier1), "\n Did you notice what changed?")
         
     }
     if (askPupular === 4){
